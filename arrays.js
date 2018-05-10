a20b84a1c82b05dfd87acd5a9bcae0844c8b4280
Juliana Kang <julianajlk@gmail.com>
2018-05-10 22:58:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+  return [...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return chocolateBars;
+}
+
+function addElementToEndOfArray (array, element) {
+  candyBars.push("reeses");
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+
+}
+
+function accessElementInArray(array, element) {
+
+}
